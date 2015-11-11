dac0aeaf13a609e444f407d924d07c774013b66e
rrazaghi <rrazaghi@ucsd.edu>
2015-11-11 03:58:38 -0800

    Revert "A code for calculating the reverse complement of a sequence was added."
    This reverts commit 546efad62996fc505fe43842de877b06b5d1cde8.

--- codontable.py
+++ codontable.py
@@ -23,5 +23,3 @@ AAnames = {'A': 'Ala','I': 'Ile', 'L': 'Leu', 'V': 'Val', 'F': 'Phe', 'W': 'Trp'
 revcomp = {'A':'T', 'T':'A', 'C':'G', 'G':'C','a':'T', 't':'A', 'c':'G', 'g':'C', 'N':'N','n':'N' } 
 
 
-def rev_seq(seq):
-	return  "".join([revcomp[base] for base in reversed(seq)])
